548edeb92390ad0e7b6fb99a2860b222b1ccc210
jeetsaha1 <jeet2022saha@gmail.com>
2025-06-15 10:21:26 +0530

    update BST

--- Codes_with_harry/Day72_binary_search_tree.c
+++ Codes_with_harry/Day72_binary_search_tree.c
@@ -38,14 +38,7 @@ int isBST(struct Node * root){
             return 0;
         }
         prev = root;
-        return isBST(root->right);if (!(isBST(root->left)))
-    {
-        return 0;
-    }
-    if (prev!=NULL && root->data <= prev->data)
-    {
-        return 0;
-    }
+        return isBST(root->right);
     }
     else
         return 1;
